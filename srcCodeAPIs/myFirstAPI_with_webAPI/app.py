c5270c35cfe625f0052b6e4594f45eb22f9a6144
vmin945 <aminag043@gmail.com>
2026-01-12 17:46:12 +0100

    mise à jour

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -4,17 +4,18 @@ app = Flask(__name__)
 @app.route('/api/salutation', methods=['GET'])
 def salutation():
     return jsonify(message="Hello World")
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 ## EXO1: API GET: renvoyer un helloworld - API end point name: "api/salutation"
+
 
 ## EXO2: API POST: renvoyer un nom fourni en parametre - API end point name: "api/utilisateurs"
 
+@app.route('/api/utilisateurs', methods=['POST'])
+def utilisateurs():
+    data = request.get_json()
+    nom = data.get("nom", "Inconnu")
+    return jsonify(message=f"Bonjour {nom}")
 # to be tested with curl: 
 # >> curl -i -X GET http://localhost:5000/api/salutation
 # >> curl -i -X POST -H 'Content-Type: application/json' -d '{"nom": "Bob"}' http://localhost:5000/api/utilisateurs
-
 if __name__ == '__main__':
     app.run(debug=True)
